3b48dc2627c2b3eed229c3c980a93cff3954d492
adil ziani el ali <adil.zianielali@zurich.com>
2024-12-20 15:26:01 +0100

    Task 9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -100,6 +100,33 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   }
 });
 
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const username = req.session.authorization.username;
+
+  if (!username) {
+    return res.status(400).json({message: "Username is required"});
+  }
+
+  // Check if the book exists
+  if (books[isbn]) {
+    // Check if the user has posted a review
+    const userReview = books[isbn].reviews[username];
+
+    if (userReview) {
+      // Delete the user's review
+      delete books[isbn].reviews[username];
+      return res.status(200).json({message: "Review deleted successfully", book: books[isbn]});
+    } else {
+      return res.status(404).json({message: "Review not found"});
+    }
+  } else {
+    return res.status(404).json({message: "Book not found"});
+  }
+
+});
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
