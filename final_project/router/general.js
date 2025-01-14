6c36ace1e878ab1e0ab461d19b02b221dd731864
adil ziani el ali <adil.zianielali@zurich.com>
2025-01-14 13:59:25 +0100

    Task 11

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,8 +1,17 @@
 const express = require('express');
 let books = require("./booksdb.js");
+const jwt = require('jsonwebtoken');//is not necesary because public_user not need to login like in /costumer
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+
+// Function to check if the user is authenticated
+const authenticatedUser = (username, password) => {
+  let validusers = users.filter((user) => {
+    return user.username === username && user.password === password;
+  });
+  return validusers.length > 0;
+};
 
 // Function to check if the user exists
 const doesExist = (username) => {
@@ -11,6 +20,7 @@ const doesExist = (username) => {
   });
   return userswithsamename.length > 0;
 };
+
 
 public_users.post("/register", (req,res) => {
   const username = req.body.username;
@@ -26,6 +36,31 @@ public_users.post("/register", (req,res) => {
   }
   return res.status(404).json({ message: "Unable to register user." });
 });
+
+//only registered users can login
+public_users.post("/login", (req,res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(404).json({ message: "Error logging in" });
+  }
+
+  if (authenticatedUser(username, password)) {
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access', { expiresIn: "1h" });
+
+    req.session.authorization = {
+      accessToken, username
+    };
+    return res.status(200).send("User successfully logged in");
+  } else {
+    return res.status(208).json({ message: "Invalid Login. Check username and password" });
+  }
+});
+// last code exists dans auth_users.js, but to acces to it, we need put /customer in the url. 
+// And it's not necesary give acces to public users by login, only register is OK, so code of /login is not important.
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
@@ -108,5 +143,27 @@ public_users.get('/',function (req, res) {
     (error) => res.send("denied")
   );  
 });
+// Task 11
+// Add the code for getting the book details based on ISBN (done in Task 2) using Promise callbacks or async-await with Axios.
+
+function getFromISBN(isbn){
+  let book_ = books[isbn];  
+  return new Promise((resolve,reject)=>{
+    if (book_) {
+      resolve(book_);
+    }else{
+      reject("Unable to find book!");
+    }    
+  })
+}
+
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn',function (req, res) {
+  const isbn = req.params.isbn;
+  getFromISBN(isbn).then(
+    (bk)=>res.send(JSON.stringify(bk, null, 4)),
+    (error) => res.send(error)
+  )
+ });
 
 module.exports.general = public_users;
